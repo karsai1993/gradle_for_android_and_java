634387f1174f19cbc3c8ca16d520a5c929459c11
Laci <karsai1993@gmail.com>
2018-05-01 00:32:35 +0200

    Added progress bar while loading joke

--- app/build/generated/source/r/free/debug/android/support/transition/R.java
+++ app/build/generated/source/r/free/debug/android/support/transition/R.java
@@ -92,21 +92,21 @@ public final class R {
         public static final int notification_main_column = 0x7f08006d;
         public static final int notification_main_column_container = 0x7f08006e;
         public static final int parent_matrix = 0x7f080073;
-        public static final int right_icon = 0x7f08007a;
-        public static final int right_side = 0x7f08007b;
-        public static final int save_image_matrix = 0x7f08007c;
-        public static final int save_non_transition_alpha = 0x7f08007d;
-        public static final int save_scale_type = 0x7f08007e;
-        public static final int status_bar_latest_event_content = 0x7f0800a1;
-        public static final int text = 0x7f0800a5;
-        public static final int text2 = 0x7f0800a6;
-        public static final int time = 0x7f0800ac;
-        public static final int title = 0x7f0800ad;
-        public static final int transition_current_scene = 0x7f0800b3;
-        public static final int transition_layout_save = 0x7f0800b4;
-        public static final int transition_position = 0x7f0800b5;
-        public static final int transition_scene_layoutid_cache = 0x7f0800b6;
-        public static final int transition_transform = 0x7f0800b7;
+        public static final int right_icon = 0x7f08007b;
+        public static final int right_side = 0x7f08007c;
+        public static final int save_image_matrix = 0x7f08007d;
+        public static final int save_non_transition_alpha = 0x7f08007e;
+        public static final int save_scale_type = 0x7f08007f;
+        public static final int status_bar_latest_event_content = 0x7f0800a2;
+        public static final int text = 0x7f0800a6;
+        public static final int text2 = 0x7f0800a7;
+        public static final int time = 0x7f0800ad;
+        public static final int title = 0x7f0800ae;
+        public static final int transition_current_scene = 0x7f0800b4;
+        public static final int transition_layout_save = 0x7f0800b5;
+        public static final int transition_position = 0x7f0800b6;
+        public static final int transition_scene_layoutid_cache = 0x7f0800b7;
+        public static final int transition_transform = 0x7f0800b8;
     }
     public static final class integer {
         public static final int cancel_button_image_alpha = 0x7f090004;
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,17 +1,31 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.VisibleForTesting;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.InterstitialAd;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+
+import java.io.IOException;
+
+import udacity.com.androidjokes.JokeActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -21,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String INTERSTITIAL_AD_WAIT_REQUEST
             = "Please, try it some moments later again!";
     private Toast mToast;
+    private ProgressBar mProgressBar;
 
     @VisibleForTesting
     @NonNull
@@ -41,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mProgressBar = findViewById(R.id.pb_loading);
         mInterstitialAd = new InterstitialAd(this);
         mInterstitialAd.setAdUnitId(getResources().getString(R.string.interstitial_ad_unit_id));
         mInterstitialAd.loadAd(new AdRequest.Builder().build());
@@ -91,6 +107,67 @@ public class MainActivity extends AppCompatActivity {
             if (mToast != null) mToast.cancel();
             mToast = Toast.makeText(this, INTERSTITIAL_AD_WAIT_REQUEST, Toast.LENGTH_LONG);
             mToast.show();
+        }
+    }
+
+    /**
+     * Created by Laci on 28/04/2018.
+     * The basic idea of this class if from here:
+     * https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/77e9910911d5412e5efede5fa681ec105a0f02ad/HelloEndpoints
+     */
+    class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
+        private MyApi myApiService = null;
+        private Context mContext;
+        private SimpleIdlingResource mIdlingResource;
+
+        public EndpointsAsyncTask(Context context, SimpleIdlingResource idlingResource) {
+            this.mContext = context;
+            this.mIdlingResource = idlingResource;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            mProgressBar.setVisibility(View.VISIBLE);
+            if (mIdlingResource != null) mIdlingResource.setIdleState(false);
+        }
+
+        @Override
+        protected String doInBackground(Void... contexts) {
+            if(myApiService == null) {
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(
+                                    AbstractGoogleClientRequest<?> abstractGoogleClientRequest
+                            ) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+                myApiService = builder.build();
+            }
+
+            try {
+                return myApiService.pullJoke().execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            mProgressBar.setVisibility(View.INVISIBLE);
+            if (mIdlingResource != null) {
+                mIdlingResource.setAsyncTaskResultJoke(result);
+                mIdlingResource.setIdleState(true);
+            }
+            Intent startJokeActivityIntent = new Intent(
+                    mContext,
+                    JokeActivity.class
+            );
+            startJokeActivityIntent.putExtra(Intent.EXTRA_TEXT, result);
+            mContext.startActivity(startJokeActivityIntent);
         }
     }
 }
